466c50b65c4b0881038375adae32d93fec853df4
fgnm <fgnm.dev@gmail.com>
2025-10-28 14:51:59 +0100

    [cd skip] update editor deps

--- src/main/games/rednblack/talos/editor/widgets/ui/MainMenu.java
+++ src/main/games/rednblack/talos/editor/widgets/ui/MainMenu.java
@@ -56,7 +56,7 @@ public class MainMenu extends Table {
             @Override
             public void clicked (InputEvent event, float x, float y) {
                 super.clicked(event, x, y);
-                VisDialog dialog = Dialogs.showOKDialog(stage.getStage(), "About Talos 1.5.0", "Talos is a an open source node based FX and Shader editor");
+                Dialogs.showOKDialog(stage.getStage(), "About Talos 1.5.2", "Talos is a an open source node based FX and Shader editor");
             }
         });
 
